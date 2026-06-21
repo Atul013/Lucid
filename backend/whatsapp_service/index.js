94a28ae9a7e33a75a16f66b7b5c9ddd742c10866
Atul013 <atulbiju13@gmail.com>
2026-06-21 11:11:52 +0530

    fix: whatsapp service loads root .env, single env for BE

--- backend/whatsapp_service/index.js
+++ backend/whatsapp_service/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: require("path").resolve(__dirname, "../../.env") });
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 const express = require("express");
